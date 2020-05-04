3b7b37445f24773f7650fb9407532d89678862b9
Martin Winandy <martin.winandy@pmwmedia.de>
2020-05-04 13:54:21 +0200

    Create Eclipse plug-in

---
+++ tinylog-demo-plugin/src/com/example/tinylog/Activator.java
@@ -0,0 +1,26 @@
+package com.example.tinylog;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	public static final String PLUGIN_ID = "tinylog-demo-plugin"; //$NON-NLS-1$
+
+	public Activator() {
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
+
+}
